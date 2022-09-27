a8fdf614c5ddc5cd6cfcc75ee36156eb9bc93540
UndeadLemon <thane.abel@gmail.com>
2022-09-27 08:57:58 -0500

    for loop basic uploaded

---
+++ fundamentals/introduction/for_loop_basic/for_loop_basic.py
@@ -0,0 +1,33 @@
+for i in range(151):
+    print(i)
+
+for i in range (0,1001,5):
+    print(i)
+
+for i in range (1,101):
+    if i%10 == 0:
+        print("Coding Dojo")
+    elif i%5 == 0:
+        print("Coding")
+    else:
+        print(i)
+# x = 0
+# for i in range (1,500000,2):
+#     x = x + i
+# print(x)
+sum = 0
+for i in range (0,500001):
+    if i%2 != 0:
+        sum += i
+
+print(sum)
+
+for i in range (2018,-1,-4):
+    print(i)
+
+def counting(lowNum, highNum, mult):
+    for i in range (lowNum, highNum):
+        if i%mult == 0:
+            print(i)
+
+counting(1,100,3)
