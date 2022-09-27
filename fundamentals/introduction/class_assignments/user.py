4b651c1c05cde4d387027f97707b61b76c5b7d21
UndeadLemon <thane.abel@gmail.com>
2022-09-27 15:18:08 -0500

    User updated

--- fundamentals/introduction/class_assignments/user.py
+++ fundamentals/introduction/class_assignments/user.py
@@ -18,34 +18,30 @@ class user:
         print(self.age)
         print(self.is_rewards_member)
         print(f"Gold card points = {self.gold_card_points}")
+        return self
 
     def enroll(self):
         if (self.is_rewards_member==False):
             self.is_rewards_member=True
             self.gold_card_points=200
+            return self
         else:
-            return True
+            print("User is already a rewards member!")
+            return self
 
     def spend_points(self, amount):
         if(self.gold_card_points < amount):
             print("User can't spend more points than they have!")
-            return False
+            return self
         else:
             self.gold_card_points-=amount
+            return self
 
 
 user_thane = user("Thane", "Abel", "thaneemail@email.com", 427, True)
 
 user_shark = user("Shark", "Of the Covenant", "sharkofthecovenant@sharks.shark", 126, False)
 
-user_shark.display_info()
+user_shark.display_info().enroll().spend_points(50)
 
-user_shark.enroll()
-
-user_shark.spend_points(50)
-
-user_shark.display_info()
-
-user_thane.spend_points(80)
-
-user_thane.display_info()
+user_thane.display_info().spend_points(80).enroll()
